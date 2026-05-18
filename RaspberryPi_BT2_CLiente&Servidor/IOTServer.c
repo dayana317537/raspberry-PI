1ae945c94a99f6da7ee6587752f436060607bb51
dayana317537 <dayana.roman.silva@gmail.com>
2026-05-18 23:32:18 +0200

    Implement statistical calculations for sensor data

--- RaspberryPi_BT2_CLiente&Servidor/IOTServer.c
+++ RaspberryPi_BT2_CLiente&Servidor/IOTServer.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <math.h> 
 #include <stdint.h>
 
 #define PORT 5000
@@ -19,13 +20,32 @@ typedef struct __attribute__((packed)) {
 } PaqueteUDP;
 
 void calcular_stats_float(float datos[], int n, char* nombre) {
-    //Faltaria realizar los calculos de media, minimos, máximos y la desviacion
-    printf("[TODO] Calcular estadísticas (float) para: %s\n", nombre);
+    float min = datos[0], max = datos[0], suma = 0.0;
+    for(int i=0; i<n; i++) {
+        if(datos[i] < min) min = datos[i];
+        if(datos[i] > max) max = datos[i];
+        suma += datos[i];
+    }
+    float media = suma / n;
+    float sum_var = 0.0;
+    for(int i=0; i<n; i++) sum_var += pow(datos[i] - media, 2);
+    float desv = sqrt(sum_var / n);
+    printf("%s \t-> Media: %6.2f | Min: %6.2f | Max: %6.2f | DesvStd: %6.2f\n", nombre, media, min, max, desv);
 }
 
 void calcular_stats_int(uint16_t datos[], int n, char* nombre) {
-    //Faltaria realizar los calculos de media, minimos, máximos y la desviacion
-    printf("[TODO] Calcular estadísticas (int) para: %s\n", nombre);
+    uint16_t min = datos[0], max = datos[0];
+    float suma = 0.0;
+    for(int i=0; i<n; i++) {
+        if(datos[i] < min) min = datos[i];
+        if(datos[i] > max) max = datos[i];
+        suma += datos[i];
+    }
+    float media = suma / n;
+    float sum_var = 0.0;
+    for(int i=0; i<n; i++) sum_var += pow(datos[i] - media, 2);
+    float desv = sqrt(sum_var / n);
+    printf("%s \t-> Media: %6.2f | Min: %6d | Max: %6d | DesvStd: %6.2f\n", nombre, media, min, max, desv);
 }
 
 int main(int argc, char *argv[]) {
@@ -50,7 +70,7 @@ int main(int argc, char *argv[]) {
     bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
     
     printf("========================================================\n");
-    printf(" Servidor IoT iniciado (Fase 1: Comunicaciones).\n");
+    printf(" Servidor IoT iniciado.\n");
     printf(" Tiempo de muestreo configurado: %d ms.\n", tiempo_a_enviar);
     printf("========================================================\n");
     
@@ -66,19 +86,29 @@ int main(int argc, char *argv[]) {
             int n_muestras = paquete->num_muestras;
             
             if (n_muestras > 0 && n_muestras <= MAX_MUESTRAS) {
-                printf("\n--- PAQUETE RECIBIDO (%d MUESTRAS) ---\n", n_muestras);
+                printf("\n--- RESULTADOS RECIBIDOS (%d MUESTRAS EN 10s) ---\n", n_muestras);
                 
-                float ax[MAX_MUESTRAS];
-                uint16_t c[MAX_MUESTRAS];
+                float ax[MAX_MUESTRAS], ay[MAX_MUESTRAS], az[MAX_MUESTRAS];
+                uint16_t r[MAX_MUESTRAS], g[MAX_MUESTRAS], b[MAX_MUESTRAS], c[MAX_MUESTRAS];
                 
                 for(int i=0; i<n_muestras; i++) {
                     ax[i] = paquete->lecturas[i].ax; 
-                    c[i] = paquete->lecturas[i].c; 
+                    ay[i] = paquete->lecturas[i].ay; 
+                    az[i] = paquete->lecturas[i].az;
+                    r[i] = paquete->lecturas[i].r; 
+                    g[i] = paquete->lecturas[i].g; 
+                    b[i] = paquete->lecturas[i].b;
+                    c[i] = paquete->lecturas[i].c; // Añadido el Clearance
                 }
                 
-                // Llamadas a las funciones stub
+                // El Servidor hace las operaciones y las imprime
                 calcular_stats_float(ax, n_muestras, "Aceleracion X");
-                calcular_stats_int(c, n_muestras, "Clearance");
+                calcular_stats_float(ay, n_muestras, "Aceleracion Y");
+                calcular_stats_float(az, n_muestras, "Aceleracion Z");
+                calcular_stats_int(r, n_muestras, "Color Rojo   ");
+                calcular_stats_int(g, n_muestras, "Color Verde  ");
+                calcular_stats_int(b, n_muestras, "Color Azul   ");
+                calcular_stats_int(c, n_muestras, "Clearance    ");
                 
                 // Enviar ACK obligatorio
                 char ack_msg[] = "ACK: Recibido";
