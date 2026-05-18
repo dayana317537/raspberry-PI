ae48bfada38f765685c50aefae793fb0a3090e93
dayana317537 <dayana.roman.silva@gmail.com>
2026-05-18 23:27:54 +0200

    Implement I2C sensor initialization and data reading

--- RaspberryPi_BT2_CLiente&Servidor/IOTClient.c
+++ RaspberryPi_BT2_CLiente&Servidor/IOTClient.c
@@ -4,15 +4,22 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
 #include <stdint.h>
 
-#define SERVER_IP "172.30.161.122" 
+#define SERVER_IP "172.30.161.122" // Asegúrate de que esta sea la IP de tu máquina virtual
 #define SERVER_PORT 5000
 #define MAX_MUESTRAS 100
 
-// TODO: Fase 2 - Definir direcciones I2C de los sensores MPU y Color
-// #define MPU_ADDR 0x68   
-// ...
+#define MPU_ADDR 0x68   
+#define REG_ACCEL 0x3B  
+#define COLOR_ADR 0x29
+#define CMD 0x80
+#define REG_ENABLE 0x00
+#define REG_ATIME  0x01
+#define REF_DATOS_COLOR 0x14 
 
 typedef struct __attribute__((packed)) {
     float ax, ay, az;
@@ -24,24 +31,48 @@ typedef struct __attribute__((packed)) {
     MuestraSensor lecturas[MAX_MUESTRAS];
 } PaqueteUDP;
 
-// int i2c_fd;
+int i2c_fd;
 
 void init_sensores() {
-    // TODO: Fase 2 - Abrir puerto /dev/i2c-1 y configurar registros del acelerómetro y sensor de color.
-    printf("[INFO] Inicialización de sensores hardware pendiente (Fase 2).\n");
+    i2c_fd = open("/dev/i2c-1", O_RDWR);
+    if (i2c_fd < 0) { perror("Error I2C"); exit(1); }
+
+    ioctl(i2c_fd, I2C_SLAVE, MPU_ADDR);
+    unsigned char cfg_accel[2] = {0x6B, 0x00};
+    write(i2c_fd, cfg_accel, 2);
+
+    ioctl(i2c_fd, I2C_SLAVE, COLOR_ADR);
+    unsigned char cfg_color_en[2] = {CMD | REG_ENABLE, 0x03};
+    write(i2c_fd, cfg_color_en, 2);
+    unsigned char cfg_color_time[2] = {CMD | REG_ATIME, 0xEB}; 
+    write(i2c_fd, cfg_color_time, 2);
 }
 
 void leer_sensores(MuestraSensor *m) {
-    // Falta implementar ioctl() y read() para obtener datos reales del bus I2C.
-    // Por ahora, rellenamos con datos de prueba para probar la comunicación con el servidor.
-    m->ax = 0.05; 
-    m->ay = -0.02; 
-    m->az = 0.98;
+    ioctl(i2c_fd, I2C_SLAVE, MPU_ADDR);
+    unsigned char reg_accel = REG_ACCEL;
+    write(i2c_fd, &reg_accel, 1);
+    unsigned char buf_accel[6];
+    read(i2c_fd, buf_accel, 6);
     
-    m->c = 1500;
-    m->r = 250;
-    m->g = 120;
-    m->b = 80;
+    int16_t raw_ax = (buf_accel[0] << 8) | buf_accel[1];
+    int16_t raw_ay = (buf_accel[2] << 8) | buf_accel[3];
+    int16_t raw_az = (buf_accel[4] << 8) | buf_accel[5];
+    
+    m->ax = raw_ax / 16384.0;
+    m->ay = raw_ay / 16384.0;
+    m->az = raw_az / 16384.0;
+
+    ioctl(i2c_fd, I2C_SLAVE, COLOR_ADR);
+    unsigned char reg_color = CMD | REF_DATOS_COLOR;
+    write(i2c_fd, &reg_color, 1);
+    unsigned char buf_color[8];
+    read(i2c_fd, buf_color, 8);
+    
+    m->c = (buf_color[1] << 8) | buf_color[0];
+    m->r = (buf_color[3] << 8) | buf_color[2];
+    m->g = (buf_color[5] << 8) | buf_color[4];
+    m->b = (buf_color[7] << 8) | buf_color[6];
 }
 
 int main() {
@@ -81,7 +112,8 @@ int main() {
         
         for(int i = 0; i < n_muestras; i++) {
             leer_sensores(&paquete.lecturas[i]);
-            printf("Muestra %d/%d tomada (Dummy).\n", i+1, n_muestras);
+            // Impresión limpia, tal y como pedías
+            printf("Muestra %d/%d tomada.\n", i+1, n_muestras);
             usleep(tiempo_ms * 1000);
         }
         
@@ -89,7 +121,6 @@ int main() {
         sendto(sockfd, &paquete, sizeof(PaqueteUDP), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
     }
     
-    close(sockfd);
-    // close(i2c_fd); // Pendiente para la fase 2
+    close(i2c_fd); close(sockfd);
     return 0;
 }
